f3389a7576a7860c652bc140835d663cfdb3a02b
k.berdelis <k.berdelis@deepsea.ai>
2022-12-05 16:23:17 +0200

    apply

--- components/DatePickers/WeekMonthPicker.tsx
+++ components/DatePickers/WeekMonthPicker.tsx
@@ -41,49 +41,97 @@ const CustomPickersDay = styled(PickersDay, {
 export const WeekMonthPicker = () => {
   const [value, setValue] = React.useState<Dayjs | null>(dayjs("2022-04-07"));
 
-  const renderWeekPickerDay = (
-    date: Dayjs,
-    selectedDates: Array<Dayjs | null>,
-    pickersDayProps: PickersDayProps<Dayjs>
-  ) => {
-    if (!value) {
-      return <PickersDay {...pickersDayProps} />;
-    }
+  const start = value?.startOf("week");
+  const end = value?.endOf("week");
+  const arr = [...Array(parseInt(end["$D"]) - parseInt(start["$D"]) + 1)];
+  console.log("arr: ", arr);
+  const renderWeekPickerDay = React.useCallback(
+    (
+      date: Dayjs,
+      selectedDates: Array<Dayjs | null>,
+      pickersDayProps: PickersDayProps<Dayjs>
+    ) => {
+      if (!value) {
+        return <PickersDay {...pickersDayProps} />;
+      }
 
-    const start = value.startOf("week");
-    const end = value.endOf("week");
+      const start = value.startOf("week");
+      const end = value.endOf("week");
 
-    const dayIsBetween = date.isBetween(start, end, null, "[]");
-    const isFirstDay = date.isSame(start, "day");
-    const isLastDay = date.isSame(end, "day");
+      const dayIsBetween = date.isBetween(start, end, null, "[]");
+      const isFirstDay = date.isSame(start, "day");
+      const isLastDay = date.isSame(end, "day");
 
-    return (
-      <CustomPickersDay
-        {...pickersDayProps}
-        disableMargin
-        dayIsBetween={dayIsBetween}
-        isFirstDay={isFirstDay}
-        isLastDay={isLastDay}
-      />
-    );
-  };
+      return (
+        // <CustomPickersDay
+        //   {...pickersDayProps}
+        //   disableMargin
+        //   dayIsBetween={dayIsBetween}
+        //   isFirstDay={isFirstDay}
+        //   isLastDay={isLastDay}
+        // />
+        // <>
+        // <span>hello my ame is</span>
+        // </>
+        <></>
+      );
+    },
+    [value]
+  );
 
+  console.log("start: ", start["$D"]);
+  console.log("end: ", end["$D"]);
+  console.log("value: ", value);
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <StaticDatePicker
-        displayStaticWrapperAs="desktop"
-        label="Week picker"
-        showDaysOutsideCurrentMonth={true}
-        value={value}
-        onChange={(newValue) => {
-          setValue(newValue);
-        }}
-        renderDay={renderWeekPickerDay}
-        renderInput={(params) => <TextField {...params} />}
-        inputFormat="'Week of' MMM d"
-      />
-    </LocalizationProvider>
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <StaticDatePicker
+          displayStaticWrapperAs="desktop"
+          label="Week picker"
+          showDaysOutsideCurrentMonth={true}
+          value={value}
+          onChange={(newValue) => {
+            setValue(newValue);
+          }}
+          // renderLoading={renderWeekPickerDay}
+          renderDay={renderWeekPickerDay}
+          renderInput={(params) => <TextField {...params} />}
+          inputFormat="'Week of' MMM d"
+        />
+      </LocalizationProvider>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-around",
+        }}>
+        {[...Array(parseInt(end["$D"]) - parseInt(start["$D"]) + 1)].map(
+          (i, index) => {
+            return <div key={index}> {parseInt(start["$D"]) + index}</div>;
+          }
+        )}
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+        }}>
+        <button
+          onClick={(newValue) => {
+            setValue(value - 7);
+          }}>
+          +
+        </button>
+        <button
+          onClick={(newValue) => {
+            setValue(newValue);
+          }}>
+          -
+        </button>
+      </div>
+    </div>
   );
 };
 
-export default WeekMonthPicker
+export default WeekMonthPicker;
